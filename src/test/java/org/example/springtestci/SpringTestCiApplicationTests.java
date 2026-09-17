cd374d9f3d237f5da2db1287d09259550cfc1eaf
wlsdn020416 <wlsdn020416@gmail.com>
2026-09-17 09:28:48 +0900

    Initial commit

---
+++ src/test/java/org/example/springtestci/SpringTestCiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springtestci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestCiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
